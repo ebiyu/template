b017a665b08d9a237b3b69a08935c159b9fce859
海老原祐輔 <ebiyuu1121@gmail.com>
2018-06-06 16:22:58 +0900

    hoge

---
+++ template.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+using namespace std;
+typedef vector<int> vint;
+typedef vector<long long> vll;
+typedef pair<int,int> pint;
+typedef long long ll;
+#define range(i,a,b) for(int i=a;i<b;++i)
+#define rep(i,n) range(i,0,n)
+#define INF 10000000000 //10^10
+#define PI 3.141592
+
+int main(){
+}
